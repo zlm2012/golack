354e4415d3ef2040ce751e5babcad3b70f82282b
Oklahomer <hagiwara.go@gmail.com>
2016-12-30 17:14:43 +0900

    Update tests for OutgoingEventID

--- rtmapi/outgoing_event_id_test.go
+++ rtmapi/outgoing_event_id_test.go
@@ -1,16 +1,26 @@
 package rtmapi
 
-import "testing"
+import (
+	"sync"
+	"testing"
+)
 
-func TestIncrementOutgoingEventID(t *testing.T) {
-	idDispenser := NewOutgoingEventID()
+func TestNewOutgoingEventID(t *testing.T) {
+	eventID := NewOutgoingEventID()
 
-	if idDispenser.id != 0 {
-		t.Errorf("id value is not starting from 0. id was %d", idDispenser.id)
+	if eventID.id != 0 {
+		t.Errorf("id value is not starting from 0. id was %d", eventID.id)
 		return
 	}
+}
 
-	nextID := idDispenser.Next()
+func TestOutgoingEventID_Next(t *testing.T) {
+	eventID := OutgoingEventID{
+		id:    0,
+		mutex: &sync.Mutex{},
+	}
+
+	nextID := eventID.Next()
 	if nextID != 1 {
 		t.Errorf("id 1 must be given on first Next() call. id was %d.", nextID)
 		return
